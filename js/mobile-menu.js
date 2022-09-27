f2fae82470dd2ffbca61bd0ab1973ecd72f7ecf4
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2022-09-27 22:17:40 +0300

    hw-8

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,10 +2,12 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileBtnClose = document.querySelector('[data-menu-close]');
-  menuBtnRef.addEventListener('click', () => {
+  menuBtnRef.addEventListener('click', e => {
     mobileMenuRef.classList.toggle('is-hidden');
+    menuBtnRef.classList.toggle('is-hidden');
   });
   mobileBtnClose.addEventListener('click', () => {
     mobileMenuRef.classList.toggle('is-hidden');
+    menuBtnRef.classList.remove('is-hidden');
   });
 })();
